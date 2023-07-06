368563edfd4b130a27ac056824cb132bdcfe4030
Slava <slavamonster6@gmail.com>
2023-07-06 17:19:15 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="content position font">
+        <?php
+            $string = <<< _STR
+             <b>Всем</b> привет, с вами <i>я</i></br>
+             Как ваши дела </br>
+             Расскажите мне
+            _STR;
+            echo $string;
+
+            $b = true;
+
+            $b ? print 'true' : print 'false';
+            $a = 5;
+            sdfdsfsd
+            $b = 5;
+            echo 'a было 5, а теперь'. ++$a.'<br>';
+            echo 'b было 5, а теперь'. $b++.'<br>';
+            //
+        ?>
+        </div>
+
+    </body>
+</html>
+
